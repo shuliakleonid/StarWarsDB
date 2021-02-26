fee38d735ab660e05358acf34491af439ea68447
Shuliak Leonid <zeleny777@gmail.com>
2021-02-26 16:14:33 +0300

    add api starWars

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  class SwapiServiceClass {
+    _apiBase = 'https://swapi.dev/api'
+
+    async getResource(url: string) {
+      const res = await fetch(`${this._apiBase}${url}`)
+      if (!res.ok) {
+        throw new Error(`Error ${url}, received ${res.status}`)
+      }
+      return await res.json()
+    }
+
+    async getAllPeople() {
+      const res = await this.getResource(`/people/`)
+      return res.results
+    }
+
+    getPerson(id: string) {
+      return this.getResource(`/people/${id}`)
+    }
+    async getAllPlanets() {
+      const res = await this.getResource('/planets/')
+      return res.results
+    }
+
+    getPlanet(id: string) {
+      return this.getResource(`/planets/${id}`)
+    }
+    async getAllStarShips() {
+      const res = await this.getResource('/starships/')
+      return res.results
+    }
+
+    getStarship(id: string) {
+      return this.getResource(`/starships/${id}`)
+    }
+  }
+  //
+  // const swapi = new SwapiServiceClass()
+  //
+  // swapi.getAllPeople().then((body) => {
+  //   body.forEach(p => console.log(p.name))
+  // })
+  // swapi.getPerson('3').then((p) => {
+  //   console.log(p.name)
+  // })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>h1</h1>
+  )
 }
 
 export default App;
